246f77e341bfe3471a3dffa05669f85c153343e0
DiskoPartizan <dmitriy.edyardovich@gmail.com>
2024-02-24 10:12:54 +0100

    task-2

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@
 // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,
 //МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
 
-const name = "ГАЛИНА";
-const age = 35;
-const language = "JAVASCRIPT";
+// const name = "ГАЛИНА";
+// const age = 35;
+// const language = "JAVASCRIPT";
 
-console.log(
-  `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ JAVASCRIPT`
-);
+// console.log(
+//   `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ JAVASCRIPT`
+// );
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , 
+// ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+const userName = prompt("введіть своє імя");
+console.log(userName)
+const userMaile = prompt("введіть свою пошту");
+const userPhone = prompt("введіть свій телефон");
+const massege = `КОРИСТУВАЧ ${ userName } ВИКОРИСТОВУЄ ${ userMaile } ПОЧТУ І ${ userPhone } ТЕЛЕФОН`;
+console.log(massege)
