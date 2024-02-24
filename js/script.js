73c4bfcbebd5b3c3cb18482185d867d2152d3d1f
Volodymyr <vyuzvyak26@gmail.com>
2024-02-24 11:29:00 +0200

    js-3

--- js/script.js
+++ js/script.js
@@ -10,11 +10,23 @@
 //   `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ JAVASCRIPT`
 // );
 // ЗАДАЧА 2
-// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , 
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ ,
 // ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
-const userName = prompt("введіть своє імя");
-console.log(userName)
-const userMaile = prompt("введіть свою пошту");
-const userPhone = prompt("введіть свій телефон");
-const massege = `КОРИСТУВАЧ ${ userName } ВИКОРИСТОВУЄ ${ userMaile } ПОЧТУ І ${ userPhone } ТЕЛЕФОН`;
-console.log(massege)
+// const userName = prompt("введіть своє імя");
+// console.log(userName);
+// const userMaile = prompt("введіть свою пошту");
+// const userPhone = prompt("введіть свій телефон");
+// const massege = `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ ${userMaile} ПОЧТУ І ${userPhone} ТЕЛЕФОН`;
+// console.log(massege);
+
+// ЗАДАЧА 3
+// ПОТРІБНО ЗАПИТАТИ У МАГАЗИНІ ЦІНУ ЗА КОЖЕН ОКРЕМИЙ ТОВАР,
+// ТА ВИВЕСТИ ПОВІДОМЛЕННЯМ  ЗАГАЛЬНУ ЦІНУ ТОВАРУ
+
+const priceDes = Number(prompt("введіть ціну компютера"));
+const pricePhone = +prompt("введіть ціну телефону");
+const priceLeptop = +prompt("введіть ціну ноутбука");
+const totalPrice = priceDes + pricePhone + priceLeptop;
+
+const massage = `Загальна ціна товару ${totalPrice}`;
+console.log(massage);
